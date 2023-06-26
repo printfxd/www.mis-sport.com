90eeecb842c40ca0a2b36f72ffc6786ca50b1fdc
printfxd <printfxd@gmail.com>
2023-06-27 01:21:09 +0800

    Add new brand "BICYCLE LINE".

---
+++ assets/js/bl-banner-2.js
@@ -0,0 +1,83 @@
+/*
+	Standout by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+*/
+
+(function($) {
+
+	// Banner.
+		var $blBanner2 = $('#blBanner2');
+
+		(function() {
+
+			// Settings.
+				var settings = {
+
+					// Images (in the format of 'url': 'alignment').
+						images: {
+							// 'https://storage.googleapis.com/mis-sport/banner/slide05.jpg': '0% 35%',
+							// 'https://storage.googleapis.com/mis-sport/banner/slide06.jpg': 'center',
+							// 'https://storage.googleapis.com/mis-sport/banner/slide07.jpg': 'center',
+							// 'https://storage.googleapis.com/mis-sport/banner/slide08.jpg': 'center'
+						},
+
+					// Delay.
+						delay: 6000
+
+				};
+
+			// Vars.
+				var	pos = 0, lastPos = 0,
+					$wrapper, $bgs = [], $bg,
+					k, v;
+
+			// Create BG wrapper, BGs.
+				$wrapper = $('<div class="bg" />')
+					.appendTo($blBanner2);
+
+				for (k in settings.images) {
+
+					// Create BG.
+						$bg = $('<div />');
+							$bg.css('background-image', 'url("' + k + '")');
+							$bg.css('background-position', settings.images[k]);
+							$bg.appendTo($wrapper);
+
+					// Add it to array.
+						$bgs.push($bg);
+
+				}
+
+			// Main loop.
+			if ($bgs.length > 0 ) {
+				$bgs[pos].addClass('visible');
+				$bgs[pos].addClass('top');
+
+				// Bail if we only have a single BG or the client doesn't support transitions.
+					if ($bgs.length == 1)
+						return;
+
+				setInterval(function() {
+
+					lastPos = pos;
+					pos++;
+
+					// Wrap to beginning if necessary.
+						if (pos >= $bgs.length)
+							pos = 0;
+
+					// Swap top images.
+						$bgs[lastPos].removeClass('top');
+						$bgs[pos].addClass('visible');
+						$bgs[pos].addClass('top');
+
+					// Hide last image after a short delay.
+						setTimeout(function() {
+							$bgs[lastPos].removeClass('visible');
+						}, settings.delay / 2);
+
+				}, settings.delay);
+			}
+		})();
+})(jQuery);
