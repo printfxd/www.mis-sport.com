706d7b7da318c52cceab2c0a54fa8a78f2867ec1
Yung-Hsiang Chiu <mirror.tw@gmail.com>
2023-07-31 08:58:11 +0800

    improve the touch zoom performance

--- assets/js/mtzoom.js
+++ assets/js/mtzoom.js
@@ -15,48 +15,45 @@ const ZoomImgClass = {
                 const imgPosX = clientX - this.parent.offsetLeft
                 const imgPosY = clientY - this.parent.offsetTop
         
-                if (imgPosX < 0 || imgPosX < 0) return
-                if (imgPosX >= this.w || imgPosY >= this.h) return
-        
                 const left = Math.ceil(imgPosX * (1 - this.SACLE))
                 const top = Math.ceil(imgPosY * (1 - this.SACLE))
         
-                this.imgEl.style.transform = `scale(${this.SACLE})`
-                this.imgEl.style.left = `${left}px`
-                this.imgEl.style.top = `${top}px`
+                this.imgEl.style.transform = `scale(${this.SACLE}) translate(${left}px,${top}px)`
             },
             onEnd: () => {
-                this.imgEl.style.transform = 'scale(1)'
-                this.imgEl.style.left = '0px'
-                this.imgEl.style.top = '0px'
+                this.imgEl.style.transform = 'scale(1) translate(0)'
             },
         }
         const createHandler = ({ onStart, onMove, onEnd }) => {
             const beg = (e) => {
-                if (onStart) onStart()
-                if (e instanceof TouchEvent && onMove) onMove(e.touches[0])
+                onStart()
+                if (onMove && e instanceof TouchEvent) onMove(e)
             }
             const mov = (e) => {
-                if (onMove) onMove(e instanceof TouchEvent ? e.touches[0] : e)
+                if (e instanceof TouchEvent) {
+                    e.preventDefault();
+                    const touches = e.changedTouches
+                    if (touches.length) onMove(touches[0])
+                } else {
+                    onMove(e)
+                }
             }
-            const end = () => {
-                if (onEnd) onEnd()
-            }
+            const end = () => onEnd()
             return {
                 touch(e) {
                     this.detach()
                     this.el = e
-                    e.addEventListener('touchstart', beg, { passive: true })
-                    e.addEventListener('touchmove', mov, { passive: true })
-                    e.addEventListener('touchend', end, { passive: true })
+                    if (onStart) e.addEventListener('touchstart', beg, { passive: true })
+                    if (onMove) e.addEventListener('touchmove', mov)
+                    if (onEnd) e.addEventListener('touchend', end, { passive: true })
                     return this
                 },
                 mouse(e) {
                     this.detach()
                     this.el = e
-                    e.addEventListener('mouseenter', beg)
-                    e.addEventListener('mousemove', mov)
-                    e.addEventListener('mouseleave', end)
+                    if (onStart) e.addEventListener('mouseenter', beg)
+                    if (onMove) e.addEventListener('mousemove', mov)
+                    if (onEnd) e.addEventListener('mouseleave', end)
                     return this
                 },
                 detach() {
