5454897c83c7b6423dfe93ee273fbed979f28bbf
printfxd <printfxd@gmail.com>
2024-09-02 15:11:24 +0800

    2024/09/02 Add 5XL into product page, and update excel data form generator commit : 13a6e7bccb81d9a82d0660ac6634202ce90bf609.

--- assets/gen/chunk.13215100.js
+++ assets/gen/chunk.13215100.js
@@ -1,6 +1,6 @@
 import { c as createAstro, a as createComponent, r as renderTemplate, d as renderComponent, m as maybeRenderHead } from './chunk.b0db1f2f.js';
 import 'clsx';
-import { a as $$Layout } from './chunk.ec27d29d.js';
+import { a as $$Layout } from './chunk.831b77c4.js';
 /* empty css                */
 var __freeze = Object.freeze;
 var __defProp = Object.defineProperty;
