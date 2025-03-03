2230ae76505f388ebb0e1e65ed9004e70aa1fe00
printfxd <printfxd@gmail.com>
2025-03-03 18:01:34 +0800

    2025/03/03 Update excel data form generator commit : 13a6e7bccb81d9a82d0660ac6634202ce90bf609.

--- assets/gen/chunk.027f894c.js
+++ assets/gen/chunk.027f894c.js
@@ -1,7 +1,7 @@
 import { c as createAstro, a as createComponent, r as renderTemplate, d as renderComponent, m as maybeRenderHead } from './chunk.b0db1f2f.js';
 import 'clsx';
-import { S as SheetToProduct, $ as $$ProductList, a as $$Layout } from './chunk.3a797df1.js';
-/* empty css                *//* empty css                */import { f as json } from './chunk.f9ab4e09.js';
+import { S as SheetToProduct, $ as $$ProductList, a as $$Layout } from './chunk.d48fde58.js';
+/* empty css                *//* empty css                */import { f as json } from './chunk.3027432a.js';
 
 var __freeze = Object.freeze;
 var __defProp = Object.defineProperty;
