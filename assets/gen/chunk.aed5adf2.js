aefb5e153db5846888a7be320cd889f82bfacd12
printfxd <printfxd@gmail.com>
2025-10-20 12:38:00 +0800

    2025/10/20 - Update excel data form generator commit : 548cc6d075c88af0c5e898e3898a9334b98ad9dd.

--- assets/gen/chunk.aed5adf2.js
+++ assets/gen/chunk.aed5adf2.js
@@ -1,7 +1,7 @@
 import { c as createAstro, a as createComponent, r as renderTemplate, d as renderComponent, m as maybeRenderHead } from './chunk.b0db1f2f.js';
 import 'clsx';
-import { S as SheetToProduct, $ as $$ProductList, a as $$Layout } from './chunk.7e68de11.js';
-/* empty css                *//* empty css                */import { d as json } from './chunk.aaef487b.js';
+import { S as SheetToProduct, $ as $$ProductList, a as $$Layout } from './chunk.1d1eb006.js';
+/* empty css                *//* empty css                */import { d as json } from './chunk.6571fafe.js';
 
 var __freeze = Object.freeze;
 var __defProp = Object.defineProperty;
--- assets/gen/chunk.e73d49c4.js
+++ assets/gen/chunk.e73d49c4.js
@@ -1,6 +1,6 @@
 import { c as createAstro, a as createComponent, r as renderTemplate, d as renderComponent, m as maybeRenderHead } from './chunk.b0db1f2f.js';
 import 'clsx';
-import { a as $$Layout } from './chunk.7e68de11.js';
+import { a as $$Layout } from './chunk.1d1eb006.js';
 /* empty css                */
 var __freeze = Object.freeze;
 var __defProp = Object.defineProperty;
